70633f3a7aff78c9b47db509e92a22d49f2a5413
EC2 Default User <ec2-user@ip-172-31-50-30.eu-north-1.compute.internal>
2025-03-29 18:28:43 +0000

    Tags

--- terraform/network/data.tf
+++ terraform/network/data.tf
@@ -5,6 +5,11 @@ locals {
   subnet_keys_with_tags = { for type, v in var.subnets : type => v.tags if can(v.tags) }
   singleton_subnet_types = ["public", "transit_gateway", "core_network"]
   private_subnet_names   = setsubtract(local.subnet_keys, local.singleton_subnet_types)
+  nat_options = {
+    "all_azs"   = local.azs
+    "single_az" = [local.azs[0]]
+    "none"      = [] # explicit "none" or omitted
+  }
   nat_gateway_configuration = try(length(var.subnets.public.nat_gateway_configuration), 0) := 0 ? var.subnets.public.nat_gateway_configuration : "none"
   nat_configuration = contains(local.subnet_keys, "public") ? local.nat_options[local.nat_gateway_configuration] : local.nat_options["none"]
 
