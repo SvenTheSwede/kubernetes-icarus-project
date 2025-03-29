9886cb384eb5eab6c062a371fa754b76ff7f67c5
EC2 Default User <ec2-user@ip-172-31-50-30.eu-north-1.compute.internal>
2025-03-29 18:27:14 +0000

    Tags

--- terraform/network/data.tf
+++ terraform/network/data.tf
@@ -1,6 +1,14 @@
 locals {
   azs = length(var.azs) == 0 ? slice(data.aws_availability_zones.current.names, 0, var.az_count) : var.azs
+  subnet_keys           = keys(var.subnets)
+  subnet_names          = { for type, v in var.subnets : type => try(v.name_prefix, type) }
+  subnet_keys_with_tags = { for type, v in var.subnets : type => v.tags if can(v.tags) }
+  singleton_subnet_types = ["public", "transit_gateway", "core_network"]
+  private_subnet_names   = setsubtract(local.subnet_keys, local.singleton_subnet_types)
+  nat_gateway_configuration = try(length(var.subnets.public.nat_gateway_configuration), 0) := 0 ? var.subnets.public.nat_gateway_configuration : "none"
+  nat_configuration = contains(local.subnet_keys, "public") ? local.nat_options[local.nat_gateway_configuration] : local.nat_options["none"]
 
-  calculated_subnets       = module.calculate_subnets.subnets_by_type
 
 }
+
+
--- terraform/network/main.tf
+++ terraform/network/main.tf
@@ -1,7 +1,7 @@
-module "calculate_subnets" {
-  source = "hashicorp/subnets/cidr"
+module "calculate_pub_subnets" {
+  source = "./modules/calculate_subnets"
 
-  base_cidr_block = "172.16.50.0/16"
+  base_cidr_block = local.cidr
   networks = [
     {
       name     = "eu-north-1a"
@@ -12,19 +12,39 @@ module "calculate_subnets" {
       new_bits = 8
     },
     {
-      name = "eu-north-1c"
+      name     = "eu-north-1c"
+      new_bits = 8
+    },
+
+  ]
+}
+
+module "calculate_priv_subnets" {
+  source = "./modules/calculate_subnets_ipv6"
+  base_cidr_block = local.priv_cidr
+  networks = [
+    {
+      name     = "eu-north-1a"
+      new_bits = 8
+    },
+    {
+      name     = "eu-norht-1b"
+      new_bits = 8
+    },
+    {
+      name     = "eu-norht-1c"
       new_bits = 8
     }
+
   ]
 }
 
 
 resource "aws_subnet" "public" {
   for_each = contains(local.subnet_keys, "public") ? toset(local.azs) : toset([])
-
   availability_zone                              = each.key
   vpc_id                                         = local.vpc.id
-  cidr_block                                     = can(local.calculated_subnets["public"][each.key]) ? local.calculated_subnets["public"][each.key] : null
+  cidr_block                                     = can(local.calculated_pub_subnets["public"][each.key]) ? local.calculated_pub_subnets["public"][each.key] : null
   map_public_ip_on_launch                        = try(var.subnets.public.map_public_ip_on_launch ? null : true)
 
   tags {
@@ -32,4 +52,30 @@ resource "aws_subnet" "public" {
      SubnetType = "Utility"
 
   }
+}
+
+resource "aws_nat_gateway" "main" {
+  for_each = toset(local.nat_configuration)
+
+  allocation_id = aws_eip.nat[each.key].id
+  subnet_id     = aws_subnet.public[each.key].id
+
+  tags = merge(
+    { Name = "nat-${local.subnet_names["public"]}-${each.key}" },
+    module.tags.tags_aws,
+    try(module.subnet_tags["public"].tags_aws, {})
+  )
+
+}
+
+
+resource "aws_eip" "nat" {
+  for_each = toset(local.nat_configuration)
+  domain   = "vpc"
+
+  tags = merge(
+    { Name = "nat-${local.subnet_names["public"]}-${each.key}" },
+    module.tags.tags_aws,
+    try(module.subnet_tags["public"].tags_aws, {})
+  )
 }
