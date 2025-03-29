e12824208696d77814b64c6adcb9527470051089
EC2 Default User <ec2-user@ip-172-31-50-30.eu-north-1.compute.internal>
2025-03-29 17:36:37 +0000

    Tags

--- terraform/network/variables.tf
+++ terraform/network/variables.tf
@@ -18,12 +18,11 @@ variable "vpc_id" {
 
 variable "subnets" {
   description = <<-EOF
-  Configuration of subnets to build in VPC. 1 Subnet per AZ is created. Subnet types are defined as maps with the available keys: "private", "public", "transit_gateway", "core_network". Each Subnet type offers its own set of available arguments detailed below. Subnets are calculated in lexicographical order of the keys in the map.
+  Configuration of subnets to build in VPC. 1 Subnet per AZ is created. Subnet types are defined as maps with the available keys: "private", "public".
 
   **Attributes shared across subnet types:**
   - `cidrs`            = (Optional|list(string)) **Cannot set if `netmask` is set.** List of IPv4 CIDRs to set to subnets. Count of CIDRs defined must match quantity of azs in `az_count`.
   - `netmask`          = (Optional|Int) **Cannot set if `cidrs` is set.** Netmask of the `var.cidr_block` to calculate for each subnet.
-  - `assign_ipv6_cidr` = (Optional|bool) **Cannot set if `ipv6_cidrs` is set.** If true, it will calculate a /64 block from the IPv6 VPC CIDR to set in the subnets.
   - `name_prefix`      = (Optional|String) A string prefix to use for the name of your subnet and associated resources. Subnet type key name is used if omitted (aka private, public, transit_gateway). Example `name_prefix = "private"` for `var.subnets.private` is redundant.
   - `tags`             = (Optional|map(string)) Tags to set on the subnet and associated resources.a "
    --- 
